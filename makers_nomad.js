9f72c8074baaeba8487510983e4a06b6c3a70f1e
yourluck <yoonchae.na@gmail.com>
2023-02-23 13:58:04 -0600

    code done

--- makers_nomad.js
+++ makers_nomad.js
@@ -1,6 +1,10 @@
 const fs = require("fs");
 const BME280 = require("bme280-sensor");
+const READ_INTERVAL = 1000; // sensor read interval for a second
+const MAX_RETRIES = 3; // max number of retries to read sensor data
+const END_COUNTER = 10; // sensor read interval for a second
 
+// configuation obj for BME280
 const options = {
   i2cBusNo: 1, // defaults to 1
   i2cAddress: BME280.BME280_DEFAULT_I2C_ADDRESS(), // defaults to 0x77
@@ -13,71 +17,43 @@ const readSensorData = () => {
   bme280
     .readSensorData()
     .then((data) => {
-      // temperature_C, pressure_hPa, and humidity are returned by default.
-      // used pre-made function to convert celsius to fahrenheit
-      data.temperature_F = BME280.convertCelciusToFahrenheit(
-        data.temperature_C
-      );
+      // function to convert celsius to fahrenheit
+      temp_fahrenheit = data.temperature_C * 1.8 + 32;
+      console.log("count :", data_count);
+      console.log("temp_c :", Math.round(data.temperature_C));
 
-      //print data to console-relieve from comment to see
-      console.log(data.temperature_C);
-      console.log(data.humidity);
+      console.log("temp_f :", Math.round(temp_fahrenheit));
+      console.log("before_temp_f :", Math.round(current_fahrenheit));
 
-      //first data
-      //# of iteration for csv line
-      let iteration = 1;
-      //temperature in celsius-from sensor
-      let current_celsius = data.temperature_C;
-      //temperature in fahrenheit-by formula
-      let current_fahrenheit = Math.round(data.temperature_F);
-      //humidity-from sensor
-      let current_humidity = Math.round(data.humidity);
-      //current time
-      let current_time;
+      console.log("humidity :", Math.round(data.humidity));
+      console.log("before_humidity :", Math.round(current_humidity));
 
-      let update_celsius; //update_temperature_celsius;
-      let update_fahrenheit; //update_temperaute_fahrenheit;
-      let update_humidity; //update_humidity;
+      error_retries = 0;
 
-      while (iteration < 10) {
-        //update_temperaute_celsius;
-        update_celsius = Math.random() + 20;
-        //update_temperaute_fahrenheit;
-        update_fahrenheit = Math.round(update_celsius * 1.8 + 32);
-        //update_humidity;
-        update_humidity = Math.round(Math.random() * 3 + 70);
-        current_time = get_time();
-
-        //if temp diff or humdity diff more than 1 F, save data
-        if (
-          Math.abs(update_fahrenheit - current_fahrenheit) >= 1 ||
-          Math.abs(update_humidity - current_humidity) >= 1
-        ) {
-          //math.abs
-          write_f(
-            file_name,
-            iteration,
-            current_time,
-            update_celsius,
-            update_fahrenheit,
-            update_humidity
-          );
-          iteration++;
-        }
-        //update sensor data
-        current_celsius = update_celsius;
-        current_fahrenheit = update_fahrenheit;
-        current_humidity = update_humidity;
-
-        sleep(1001);
+      if (
+        data_count == 0 ||
+        Math.abs(temp_fahrenheit - current_fahrenheit) >= 1 ||
+        Math.abs(data.humidity - current_humidity) >= 1
+      ) {
+        data_count++;
+        write_f(file_name, data);
+        current_fahrenheit = temp_fahrenheit;
+        current_humidity = data.humidity;
       }
 
-      //console.log(`data = ${JSON.stringify(data, null, 2)}`);
-      //setTimeout(readSensorData, 2000);
+      if (data_count < END_COUNTER) {
+        setTimeout(readSensorData, READ_INTERVAL);
+      }
     })
     .catch((err) => {
       console.log(`BME280 read error: ${err}`);
-      setTimeout(readSensorData, 2000);
+      if (error_retries++ < MAX_RETRIES) {
+        setTimeout(readSensorData, READ_INTERVAL);
+      } else {
+        console.error(
+          "BME280 read error: max retries(3) exceeded, Check the connected h/w!"
+        );
+      }
     });
 };
 
@@ -90,43 +66,21 @@ function get_time() {
   var hh = date.getHours();
   var mm = date.getMinutes();
   var ss = date.getSeconds();
-  return (
-    "" +
-    year +
-    (month < 10 ? "0" + month : month) +
-    (day < 10 ? "0" + day : day) +
-    (hh < 10 ? "0" + hh : hh) +
-    (mm < 10 ? "0" + mm : mm) +
-    (ss < 10 ? "0" + ss : ss)
-  );
+  return `${year}${month < 10 ? "0" + month : month}${
+    day < 10 ? "0" + day : day
+  }${hh < 10 ? "0" + hh : hh}${mm < 10 ? "0" + mm : mm}${
+    ss < 10 ? "0" + ss : ss
+  }`;
 }
 
-//sleep function-delay ex-2s
-function sleep(delay) {
-  var start = new Date().getTime();
-  while (new Date().getTime() < start + delay);
-}
 //write data to file
-function write_f(
-  file_name,
-  iteration,
-  time,
-  temperature_celsius,
-  temperature_fahrenheit,
-  humidity
-) {
-  temperature_celsius = Math.round(temperature_celsius * 100) / 100;
-  one_row =
-    iteration +
-    "," +
-    time +
-    "," +
-    temperature_celsius +
-    "," +
-    temperature_fahrenheit +
-    "," +
-    humidity +
-    "\n";
+function write_f(file_name, data) {
+  // # of data, time, temperature_celsius, temperature_fahrenheit, humidity
+  const one_row = `${data_count},${get_time()},${
+    Math.round(data.temperature_C * 100) / 100
+  },${Math.round(data.temperature_C * 1.8 + 32)},${Math.round(
+    data.humidity
+  )}\n`;
 
   fs.appendFileSync(file_name, one_row, (err) => {
     if (err) {
@@ -137,14 +91,27 @@ function write_f(
 }
 
 //create csv file and write header
-head_row = "#,time,temperature_celsius,temperature_fahrenheit,humidity\n";
-file_name = "senor_data" + get_time() + ".csv";
+const head_row = "#,time,temperature_celsius,temperature_fahrenheit,humidity\n";
+const file_name = "senor_data" + get_time() + ".csv";
 fs.appendFileSync(file_name, head_row, (err) => {
   if (err) {
     console.error(err);
     return;
   }
 });
+
+//# of data for csv line
+let data_count = 0;
+
+// # of consecutive sensor errors, reset when once sensor data is read
+let error_retries = 0;
+
+//temperature in fahrenheit-by formula
+let current_fahrenheit;
+// calculated temperature in fahrenheit by data.temperature_C
+let temp_fahrenheit;
+//humidity-from sensor
+let current_humidity;
 
 bme280
   .init()
